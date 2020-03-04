ac6913c8eee33dc7d81250e16d7ca8ac00ddbb1b
stuartjshepherd <stuart@createstudios.com.au>
2020-03-05 08:26:17 +1100

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Delegate
+{
+    class Program
+    {
+        public delegate double MathDelegate(double value1, double value2);
+
+        public static double Add(double value1, double value2)
+        {
+            return value1 + value2;
+        }
+        public static double Subtract(double value1, double value2)
+        {
+            return value1 - value2;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Delegate");
+            Console.WriteLine("A delegate is a type that defines a method signature. You can invoke the method through the delegate.");
+            Console.WriteLine();
+
+            // output result is 7      
+            MathDelegate mathAddDelegate = Add;
+            Console.WriteLine(mathAddDelegate(5, 2));
+
+            // output result is 3
+            MathDelegate mathSubtractDelegate = Subtract;
+            Console.WriteLine(mathSubtractDelegate(5, 2));           
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Delegate Example
+A delegate is a type that defines a method signature. You can invoke the method through the delegate.
+
+## Getting Started
+
+### Prerequisites
+
+[.NET Core 3.1 SDK or later](https://dotnet.microsoft.com/download/dotnet-core/3.1)
